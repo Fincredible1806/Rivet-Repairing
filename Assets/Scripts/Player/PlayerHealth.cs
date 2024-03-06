012e56d9773776acc010c4eb6354202319a0bb65
Finlay John <55362001+Fincredible1806@users.noreply.github.com>
2024-03-06 10:31:01 +0000

    HealthSetup

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] private Slider healthSlider;
     public int health;
     public bool dead;
     public void TakeDamage(int damage)
     {
         health -= damage;
-
+        healthSlider.value = health;
         if(health <= 0)
         {
             dead = true;
