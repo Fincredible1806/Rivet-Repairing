85586ba3d5d19a2e0d61d1562d181173b92310c8
Finlay John <findominator7@gmail.com>
2024-03-23 13:53:12 +0000

    Important stuff

--- Assets/Scripts/WorldSTuff/HealShower.cs
+++ Assets/Scripts/WorldSTuff/HealShower.cs
@@ -9,11 +9,31 @@ public class HealShower : MonoBehaviour
     [SerializeField] PlayerHealth playerHealth;
     [SerializeField] Transform particleSpawnLocation;
     [SerializeField] int healValue;
+    private GameObject spawnedParticles;
+    [SerializeField] private float useTime;
+    [SerializeField] private float timePassed;
+    private bool isRebooting;
 
+    private void Update()
+    {
+        if(isRebooting)
+        {
+            timePassed += Time.deltaTime;
+            Debug.Log("Time Passed " + timePassed);
+            if(timePassed >= useTime)
+            {
+                timePassed = 0;
+                isRebooting = false;
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        Instantiate(healParticles, particleSpawnLocation);
+        if (!isRebooting)
+        spawnedParticles = Instantiate(healParticles, particleSpawnLocation);
         playerHealth.TakeDamage(-healValue);
+        Destroy(spawnedParticles, 100 * Time.deltaTime);
+        isRebooting = true;
     }
 
 }
